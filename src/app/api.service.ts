aec81ef7284ed8ca4dafe6c1dd4b2a6e584696da
Brdej <grzegorz.brdej@solarwinds.com>
2017-03-30 10:56:01 +0200

    fix #3

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 import { MovieModel } from './models/movie.model';
 import { CategoryModel } from './models/category.model';
